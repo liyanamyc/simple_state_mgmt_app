782d167b720fcaa9e9ea9051e24416b17e2cdb9b
liyanamyc <liyana.myc@gmail.com>
2022-09-26 16:01:28 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/simple_state_mgmt_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simple_state_mgmt_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_state_mgmt_app/common/theme.dart';
+import 'package:simple_state_mgmt_app/screen/cart.dart';
+import 'package:simple_state_mgmt_app/screen/catalog.dart';
+import 'package:simple_state_mgmt_app/screen/login.dart';
+import 'model/cart.dart';
+import 'model/catalog.dart';
+
+void main() {
+  setupWindow();
+  runApp(const MyApp());
+}
+
+const double windowWidth = 400;
+const double windowHeight = 800;
+
+void setupWindow() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    // setWindowTitle('Provider Demo');
+    // setWindowMinSize(const Size(windowWidth, windowHeight));
+    // setWindowMaxSize(const Size(windowWidth, windowHeight));
+    // getCurrentScreen().then((screen) {
+    //   setWindowFrame(Rect.fromCenter(
+    //     center: screen!.frame.center,
+    //     width: windowWidth,
+    //     height: windowHeight,
+    //   ));
+    // });
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Using MultiProvider is convenient when providing multiple objects.
+    return MultiProvider(
+      providers: [
+        // In this sample app, CatalogModel never changes, so a simple Provider
+        // is sufficient.
+        Provider(create: (context) => CatalogModel()),
+        // CartModel is implemented as a ChangeNotifier, which calls for the use
+        // of ChangeNotifierProvider. Moreover, CartModel depends
+        // on CatalogModel, so a ProxyProvider is needed.
+        ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+          create: (context) => CartModel(),
+          update: (context, catalog, cart) {
+            if (cart == null) throw ArgumentError.notNull('cart');
+            cart.catalog = catalog;
+            return cart;
+          },
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Provider Demo',
+        theme: appTheme,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const MyLogin(),
+          '/catalog': (context) => const MyCatalog(),
+          '/cart': (context) => const MyCart(),
+        },
+      ),
+    );
+  }
+}
